35a6cf6e8c99a90b90f31780983c998821881024
Shandy Casteel <shandycasteel@gmail.com>
2019-10-22 11:57:23 -0500

    Initialize Spring Boot project, connect to remote repo

---
+++ src/main/java/com/shandycasteel/diderot/DiderotApplication.java
@@ -0,0 +1,13 @@
+package com.shandycasteel.diderot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiderotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiderotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shandycasteel/diderot/DiderotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shandycasteel.diderot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiderotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
