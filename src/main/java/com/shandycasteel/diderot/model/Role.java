34c41ef26312631cc3640f0e175c5d1edaa2d9eb
Shandy Casteel <shandycasteel@gmail.com>
2019-11-15 23:56:58 -0600

    Create RoleRepository

--- src/main/java/com/shandycasteel/diderot/model/Role.java
+++ src/main/java/com/shandycasteel/diderot/model/Role.java
@@ -1,9 +1,22 @@
 package com.shandycasteel.diderot.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private Set users;
+
+    @ManyToMany(mappedBy = "roles")
+    public  Set getUsers() {
+        return users;
+    }
 
 }
--- src/main/java/com/shandycasteel/diderot/repository/UserRepository.java
+++ src/main/java/com/shandycasteel/diderot/repository/UserRepository.java
@@ -5,10 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-
-    @Override
-    void delete(User user);
+public interface UserRepository extends JpaRepository {
+    User findByUsername(String username);
 
 }
