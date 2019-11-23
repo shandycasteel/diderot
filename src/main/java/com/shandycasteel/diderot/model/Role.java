562b15be7f010ca9f723bcd88b10a02472e066cf
Shandy Casteel <shandycasteel@gmail.com>
2019-11-23 16:59:53 -0600

    Refactor Role class

--- src/main/java/com/shandycasteel/diderot/model/Role.java
+++ src/main/java/com/shandycasteel/diderot/model/Role.java
@@ -12,11 +12,32 @@ public class Role {
     private Long id;
 
     private String name;
-    private Set users;
 
     @ManyToMany(mappedBy = "roles")
-    public  Set getUsers() {
+    private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<User> getUsers() {
         return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 
 }
