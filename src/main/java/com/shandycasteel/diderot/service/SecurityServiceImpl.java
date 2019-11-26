a0ea297a7e6bf8467d039dacab369dac70285a40
Shandy Casteel <shandycasteel@gmail.com>
2019-11-25 23:56:18 -0600

    Refactor SecuirtyServiceImpl

--- src/main/java/com/shandycasteel/diderot/service/SecurityServiceImpl.java
+++ src/main/java/com/shandycasteel/diderot/service/SecurityServiceImpl.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,15 @@ public class SecurityServiceImpl implements SecurityService {
     private UserDetailsService userDetailsService;
 
     private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+    @Override
+    public String findLoggedInEmail() {
+        Object userDetails = SecurityContextHolder.getContext().getAuthentication().getDetails();
+        if (userDetails instanceof UserDetails) {
+            return ((UserDetails)userDetails).getEmail();
+        }
+
+        return null;
+    }
 
 }
